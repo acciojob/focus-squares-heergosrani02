60ce7756866ede9180260c47bb0df248b7128922
Heer Gosrani <106440873+heergosrani02@users.noreply.github.com>
2025-03-01 18:23:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let squares = document.querySelectorAll(".square");
+
+squares.forEach((square) => {
+	square.addEventListener("mouseover", (event) => {
+		squares.forEach((sq) => {
+			sq.style.backgroundColor = '#6F4E37';
+		});
+		event.target.style.backgroundColor = '#E6E6FA';
+	});
+
+	square.addEventListener("mouseout", () => {
+		squares.forEach((sq) => {
+			sq.style.backgroundColor = '#E6E6FA';
+		});
+	});
+});
